c41dc1ab9b3f7d9ac36c8da8f6bf51f19c66fc90
Palash Bansal <palashbansal96@gmail.com>
2024-08-29 18:36:29 +0530

    Basic like and follow notifications.

--- starter.sql
+++ starter.sql
@@ -90,6 +90,19 @@ create table public.project_versions
     project_data jsonb                                      not null             default '{}'::jsonb
 );
 
+-- Create table for notifications. todo - notifications not updated for the last 3(or 7) days should be deleted.
+create table public.user_notifications
+(
+    id         uuid                     not null primary key default extensions.uuid_generate_v4(),
+    user_id    uuid references auth.users on delete cascade not null,
+    project_id uuid references projects on delete cascade default null,
+    updated_at timestamp with time zone default now() not null,
+    type       text                     not null, -- 'like', 'comment', 'follow', 'mention'
+    users_ref  uuid[]                   not null, -- user_id of the users involved
+    data       jsonb                    not null default '{}'::jsonb
+--     unique (user_id, project_id, type)
+);
+
 -- Create table for user assets
 create table public.user_assets
 (
@@ -126,6 +139,8 @@ alter table project_versions
 alter table user_follows
     enable row level security;
 alter table user_assets
+    enable row level security;
+alter table user_notifications
     enable row level security;
 
 -- endregion
@@ -383,6 +398,26 @@ begin
     where id = project_id
       and tag = any (tags)
       and (owner_id = auth.uid() or auth.uid() = any (editors));
+end;
+$$ language plpgsql security definer;
+
+-- Trigger to notify user when a project is liked or user is followed
+create or replace function public.notify_user(i_project_id uuid, o_user_id uuid, i_user_id uuid, i_type text)
+    returns void as
+$$
+begin
+    -- auth.uid() should be i_user_id (the user who liked/followed) since its definer
+    if auth.uid() is null or auth.uid() != i_user_id then
+        raise exception 'User is not authenticated';
+    end if;
+    -- or insert new notification. no need for data.
+    -- update notification if already exists(in last 3 days), adding user_id to users_ref
+    insert into public.user_notifications (user_id, project_id, type, users_ref)
+    values (o_user_id, i_project_id, i_type, array[i_user_id]::uuid[])
+    on conflict (user_id, project_id, type) -- where updated_at > now() - interval '3 days' -- todo test this...
+        do update set users_ref = array_append(user_notifications.users_ref, i_user_id);
+
+--   todo  perform pg_notify('notification', jsonb_build_object('type', 'like', 'notification_id', notification_id)::text);
 end;
 $$ language plpgsql security definer;
 
@@ -857,6 +892,12 @@ create trigger handle_updated_at_profiles
     for each row
 execute procedure extensions.moddatetime(updated_at);
 
+create trigger handle_updated_at_notifications
+    before update
+    on public.user_notifications
+    for each row
+execute procedure extensions.moddatetime(updated_at);
+
 create trigger handle_updated_at_projects
     before update
     on public.projects
@@ -893,6 +934,36 @@ create trigger update_like_count_trigger
     for each row
 execute procedure public.update_project_like_count();
 
+create or replace function public.notify_project_like()
+    returns trigger as
+$$
+begin
+    select public.notify_user(NEW.project_id, (select owner_id from projects where id = NEW.project_id), NEW.user_id, 'like');
+    return new;
+end;
+$$ language plpgsql security invoker;
+
+create trigger notify_project_like_trigger
+    after insert
+    on public.project_likes
+    for each row
+execute procedure public.notify_project_like();
+
+create or replace function public.notify_user_follow()
+    returns trigger as
+$$
+begin
+    select public.notify_user(null, NEW.user_id, NEW.follower_id, 'follow');
+    return new;
+end;
+$$ language plpgsql security invoker;
+
+create trigger notify_user_follow_trigger
+    after insert
+    on public.user_follows
+    for each row
+execute procedure public.notify_user_follow();
+
 -- endregion
 
 -- region Create policies for RLS SELECT access
@@ -911,6 +982,9 @@ create policy "User assets can be seen if public or user has project access" on 
     (is_private = false
         or (owner_id is not null and auth.uid() = owner_id)
         or can_user_access_project_id(project_id));
+
+create policy "Users can read their notifications" on public.user_notifications
+    for select to authenticated using (auth.uid() = user_id);
 
 create policy "Users can like projects" on public.project_likes
     for insert to authenticated with check (auth.uid() = user_id);
@@ -947,6 +1021,15 @@ create index on user_assets (is_private);
 create index on user_assets (asset_type);
 
 create index on profiles (username);
+
+-- create index on project_likes (project_id);
+-- create index on project_likes (user_id);
+--
+-- create index on user_follows (follower_id);
+-- create index on user_follows (user_id);
+--
+-- create index on notifications (user_id);
+
 
 -- endregion
 
